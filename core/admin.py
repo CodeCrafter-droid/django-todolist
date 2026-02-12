81fa528d3a330c3cd1f08aaaa490317cb27188af
sahas <xyz.@gmail.com>
2026-02-12 12:00:27 +0530

    added new column in admin panel taskdata

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import *
-from django.urls import reverse
 # Register your models here.
 
-admin.site.register(taskdata)
+class TaskDataAdmin(admin.ModelAdmin):
+    list_display = ('task','user','created_at','updated_at','user_is_superuser')
+    def user_is_superuser(self, obj):
+        return obj.user.is_superuser
+
+    user_is_superuser.boolean = True 
+    user_is_superuser.short_description = "Is Superuser"
+    
+admin.site.register(taskdata, TaskDataAdmin)
 
 admin.site.site_url = "/home"
